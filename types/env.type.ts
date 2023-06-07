65a2c0d7a98e32eb5c9cf474dcc9c685c0da4a63
José David Melián Villalobos <josedavidmelian@gmail.com>
2023-06-07 20:43:28 +0100

    Adding the final changes and documentation

--- types/env.type.ts
+++ types/env.type.ts
@@ -1,3 +1,4 @@
+//model for the variables inside the .env that will be use into the test configuration
 export type EnvConfigType = {
     mainUrl: string,
     cookieConsent: string,
--- types/userCredentials.type.ts
+++ types/userCredentials.type.ts
@@ -1,3 +1,4 @@
+//model for the variables inside the .env that will be used like user credentials inside the tests
 export type userCredentialsType = {
     username: string,
     password: string
